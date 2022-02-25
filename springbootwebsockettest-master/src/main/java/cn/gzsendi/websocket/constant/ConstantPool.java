5e2b9a1ba0ee1466a0c82005206602236a5bb282
c965898749 <965898749@qq.com>
2022-02-25 09:37:39 +0800

    远程协助

---
+++ springbootwebsockettest-master/src/main/java/cn/gzsendi/websocket/constant/ConstantPool.java
@@ -0,0 +1,12 @@
+package cn.gzsendi.websocket.constant;
+
+/**
+* @Description: 常量池
+*/
+public class ConstantPool {
+    /**
+     * 随机生成uuid的key名
+     */
+    public static final String USER_UUID_KEY = "user_uuid";
+
+}
